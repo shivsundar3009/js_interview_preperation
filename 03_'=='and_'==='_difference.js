926c5fe31ff7522d17f42bb15036c3d61d5b2180
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2024-01-01 21:20:47 +0530

    Create 03_'=='and_'==='_difference.js

---
+++ 03_'=='and_'==='_difference.js
@@ -0,0 +1,3 @@
+In JavaScript, '==' is the equality operator, which checks if the values are equal after type coercion, 
+  while '===' is the strict equality operator, which checks both value and type
+  , without performing type coercion.
