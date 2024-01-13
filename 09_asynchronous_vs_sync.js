dcfa2788b4d16b5996a0836f1f2dbbab411e5e60
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2024-01-13 22:52:27 +0530

    Update 09_asynchronous_vs_sync.js

--- 09_asynchronous_vs_sync.js
+++ 09_asynchronous_vs_sync.js
@@ -1,4 +1,5 @@
-Asynchronous and synchronous are terms used to describe the way in which tasks are executed in a programming language, and they are often discussed in the context of JavaScript.
+Asynchronous and synchronous are terms used to describe the way in which tasks are executed in a programming language, 
+and they are often discussed in the context of JavaScript.
 
 1. Synchronous JavaScript:
 
@@ -13,3 +14,53 @@ Synchronous code can be more straightforward to read and write, as the flow of e
 console.log('Task 1');
 console.log('Task 2');
 console.log('Task 3');
+
+2. Asynchronous JavaScript:
+
+In asynchronous code, tasks are initiated, but the program doesn't wait for them to complete. Instead, it continues with other tasks.
+  
+Asynchronous operations are typically used for tasks that might take some time, such as fetching data from a server or reading a file.
+  
+Callbacks, Promises, and async/await are common mechanisms for handling asynchronous code in JavaScript.
+
+### using callback
+
+  console.log('Task 1');
+
+setTimeout(function() {
+    console.log('Task 2');
+}, 1000);
+
+console.log('Task 3');
+
+### using Promise
+
+console.log('Task 1');
+
+new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        console.log('Task 2');
+        resolve();
+    }, 1000);
+}).then(function() {
+    console.log('Task 3');
+});
+
+### using async Await
+
+  async function example() {
+    console.log('Task 1');
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    console.log('Task 2');
+}
+
+example();
+console.log('Task 3');
+
+Asynchronous code is crucial in scenarios where you want to avoid blocking the main thread, 
+  
+especially in web development when dealing with tasks like making API calls or handling user 
+
+interactions without freezing the UI. It allows for more responsive and efficient applications.
