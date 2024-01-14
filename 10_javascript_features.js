65c6b474fd379691b5528014df6776bdb5add8e9
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2024-01-14 23:15:19 +0530

    Create 10_javascript_features.js

---
+++ 10_javascript_features.js
@@ -0,0 +1,8 @@
+1. what is JavaScript?
+
+JavaScript is a high-level, interpreted programming language that is widely used for creating interactive and dynamic web pages.
+
+
+2. What are the key features of JavaScript?
+
+Dynamic typing, Prototype-based inheritance, First-class functions, Closures, Event-driven programming, and Asynchronous programming with callbacks.
